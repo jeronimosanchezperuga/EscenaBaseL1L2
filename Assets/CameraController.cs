c3c8ebed45efd65343ec51897bfc3d907d4c8c45
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-11-14 11:47:34 -0300

    character browser camera

---
+++ Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraTransform.Translate(Vector3.right * movementSpeed * Input.GetAxis("Horizontal"));
+
+
+    }
+}
