1486f46f538eb86e337fdd451201e8c5bd531541
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-12-01 14:32:47 -0300

    camas

---
+++ Assets/Rebotin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rebotin : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.SetTrigger("Saltar");
+    }
+
+
+}
